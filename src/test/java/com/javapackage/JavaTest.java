9e24b0c952ec0e33efa9b1e1629a398c349f7464
Chirag Makkar <chiragmakkar2@gmail.com>
2019-04-04 17:07:34 +0530

    Changes Done In Manifest

--- src/test/java/com/javapackage/JavaTest.java
+++ src/test/java/com/javapackage/JavaTest.java
@@ -13,14 +13,11 @@ import org.junit.Test;
  */
 public class JavaTest {
 
-	@Test
-	public void testOdd() {
-		JavaRun test = new JavaRun();
-        assertEquals("20 is a odd number", false, test.isEvenNumber(21));
-	}
-	@Test
-    public void testEven(){
-        JavaRun meo = new JavaRun();
-        assertEquals("10 is a even number", true, meo.isEvenNumber(10));
-    }
+	
+	  @Test public void testOdd() { JavaRun test = new JavaRun();
+	  assertEquals("20 is a odd number", false, test.isEvenNumber(21)); }
+	  
+	  @Test public void testEven(){ JavaRun meo = new JavaRun();
+	  assertEquals("10 is a even number", true, meo.isEvenNumber(10)); }
+	 
 }
